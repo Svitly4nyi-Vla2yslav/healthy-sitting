ef7d13932f11c34843d9bede5c055bbed8a6c45b
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-07-06 13:27:06 +0200

    route shanged

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
-        },
-      },
-      base: '/',
-    });
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets'
+    },
+  },
+  build: {
+    outDir: 'dist', // переконайтесь, що тут вказаний правильний каталог
+  },
+  base: '/',
+});
