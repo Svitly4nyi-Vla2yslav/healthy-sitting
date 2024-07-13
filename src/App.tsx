42a12824dd1869737445a71db543e6d867be8bef
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-07-13 14:16:29 +0200

    add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+import ProjectPage from './pages/ProjectPage/ProjectPage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import { AppWrapper } from './App.styled';
+import Home from './pages/HomePage/HomePage';
+import RewardsPage from './pages/RewardsPage/RewardsPage';
+import ContactPage from './pages/ContactPage/ContactPage';
+import AboutUs from './pages/AboutUs/AboutUs';
+// import Background from './components/Background/Background';
+
+const App: React.FC = () => {
+  return (
+    <AppWrapper>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="project" element={<ProjectPage />} />
+          <Route path="rewards" element={<RewardsPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </AppWrapper>
+  );
+};
+
+export default App;
