050198f46574f4fd1a306e320020c66f12ba520b
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-08-07 21:04:56 +0000

    Deploying to gh-pages from @ Svitly4nyi-Vla2yslav/healthy-sitting@b16363e735fc2d0b279c61cabf5bfb5937f144ea 🚀

---
+++ assets/ProjectPage-c782c99b.js
@@ -0,0 +1,47 @@
+import{n as t,a as n,j as e}from"./home-page-66336761.js";import"./react-vendor-742c876a.js";const r="/assets/Terminator-product-section_8-2380x850-3-9ab058dd.webp",o=t.div`
+ background-image: url(${r});
+   width: 100%;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+`,i=t.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+     padding-top: 100px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    min-width: 330px;
+    margin : 0 auto;
+    pading: 5px;
+`,a=t.div`
+  text-align: center;
+`,c=t.h1`
+  color: #ffffff;
+  background: linear-gradient(90deg, #ff7f50, #1e90ff);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+`;t(n)`
+  padding: 8px 16px;
+  color: #ffffff; /* White text color */
+  transition:
+    color 0.3s,
+     background 0.3s;
+  display: block;
+  &:hover,
+  &:focus {
+     background: linear-gradient(-90deg, #ff7f50, #1e90ff);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+  }
+`;const s=()=>e.jsx(o,{children:e.jsx(i,{children:e.jsx(a,{children:e.jsx(c,{children:"Projects "})})})});export{s as default};
