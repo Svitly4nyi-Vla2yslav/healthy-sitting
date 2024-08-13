1edf6f07db8ff1db20dc339c204954ca604c8cbd
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-08-13 13:57:11 +0200

    SSH

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -16,24 +16,30 @@ const ErrorPage: React.FC = () => {
   return (
     <Wrapper
       style={{
-        height: "100%"
+        height: '100%',
       }}
     >
-      <VideoContainer  style={{
-       width: '100%',
-      position: "fixed"
-      }}>
-        <video autoPlay loop muted style={{objectFit: 'cover', position: "fixed"}}>
+      <VideoContainer
+        style={{
+          width: '100%',
+          position: 'fixed',
+        }}
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          style={{ objectFit: 'cover', position: 'fixed' }}
+        >
           <source src={Video} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </VideoContainer>
-      <ContentOverlay style={{marginTop: 10 }}>
+      <ContentOverlay style={{ marginTop: 10 }}>
         <LanguageMenu />
         <h1 style={{ fontSize: '3.5em' }}>{t('ErrorPage.CommingSoon')}</h1>
         <p style={{ fontSize: '1.5em' }}>{t('ErrorPage.ThankYou')}</p>
         <Subscribe /> <CountdownTimer targetDate={targetDate} />
-       
       </ContentOverlay>
     </Wrapper>
   );
