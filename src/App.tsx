254e6d4581309f9044eb79fe54babeb45530e6f1
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-08-16 15:49:39 +0200

    new App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,34 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-// import ProjectPage from './pages/ProjectPage/ProjectPage';
+import ProjectPage from './pages/ProjectPage/ProjectPage';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import { AppWrapper } from './App.styled';
 import React from 'react';
-// import Home from './pages/HomePage/HomePage';
+import Home from './pages/HomePage/HomePage';
 // import RewardsPage from './pages/RewardsPage/RewardsPage';
-// import ContactPage from './pages/ContactPage/ContactPage';
+import ContactPage from './pages/ContactPage/ContactPage';
 // import AboutUs from './pages/AboutUs/AboutUs';
 // import Background from './components/Background/Background';
-const Home = React.lazy(() => import('./pages/HomePage/HomePage'));
-const ContactPage = React.lazy(() => import('./pages/ContactPage/ContactPage'));
-const ProjectPage = React.lazy(() => import('./pages/ProjectPage/ProjectPage'));
+// const Home = React.lazy(() => import('./pages/HomePage/HomePage'));
+// const ContactPage = React.lazy(() => import('./pages/ContactPage/ContactPage'));
+// const ProjectPage = React.lazy(() => import('./pages/ProjectPage/ProjectPage'));
 // const RewardsPage = React.lazy(() => import('./pages/RewardsPage/RewardsPage'));
 // const AboutUs = React.lazy(() => import('./pages/AboutUs/AboutUs'));
 
 const App: React.FC = () => {
   return (
     <AppWrapper>
-        <Routes>
-          {/* Основний редирект на /home */}
-          <Route path="/" element={<Navigate to="/home" />} />
-
-          {/* SharedLayout обгортає всі підшляхи */}
-          <Route path="/" element={<SharedLayout />}>
-            <Route path="/home" element={<Home />} />
-            <Route path="/project" element={<ProjectPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            {/* Можна додати інші маршрути тут */}
-          </Route>
-
-          {/* Обробка невизначених маршрутів */}
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
+          <Routes>
+        <Route path="/" element={<Navigate to="home" />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="home" element={<Home />} />
+          {/* <Route path="about" element={<AboutUs />} /> */}
+          <Route path="project" element={<ProjectPage />} />
+          {/* <Route path="rewards" element={<RewardsPage />} /> */}
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </AppWrapper>
   );
 };
