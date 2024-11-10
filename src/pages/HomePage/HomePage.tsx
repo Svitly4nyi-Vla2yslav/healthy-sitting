6326bbbb0e76b75ca5cec852a2aa9f4af5230d20
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-11-10 17:27:29 +0100

    delete coment

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,21 +3,11 @@ import { Container } from './HomePage.styled';
 
 import ContentHomePage from '../../components/ContentHomePage/ContentHomePage';
 import Background from '../../components/Background/Background';
-// import HelpCards from '../../components/HelpUs/HelpCards';
 import ITgame from '../../components/ITsection/ITgame';
 import ITswiper from '../../components/ITsection/ITswiper';
 import News from '../../components/News/News';
 import Partners from '../../components/Partners/Partners';
 import Feedback from '../../components/Feedback/Feedback';
-
-// const ContentHomePage = React.lazy(() => import('../../components/ContentHomePage/ContentHomePage'));
-// const Background = React.lazy(() => import('../../components/Background/Background'));
-// const HelpCards = React.lazy(() => import('../../components/HelpUs/HelpCards'));
-// const ITgame = React.lazy(() => import('../../components/ITsection/ITgame'));
-// const ITswiper = React.lazy(() => import('../../components/ITsection/ITswiper'));
-// const News = React.lazy(() => import('../../components/News/News'));
-// const Partners = React.lazy(() => import('../../components/Partners/Partners'));
-// const Feedback = React.lazy(() => import('../../components/Feedback/Feedback'));
 
 const Home: React.FC = () => {
   return (
